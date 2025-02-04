7a0f2ec8e7b145c534a901a03f5d62dc71345004
J-Huster <139395235+J-Huster@users.noreply.github.com>
2025-02-04 10:48:10 -0700

    Denver Weather WebScraping Python fiel

--- weatherscrape.py
+++ weatherscrape.py
@@ -6,29 +6,56 @@
 import requests
 from bs4 import BeautifulSoup
 import time
+from datetime import date
+from datetime import datetime
 url = "https://forecast.weather.gov/MapClick.php?lat=39.6127&lon=-105.0162"
 page = requests.get(url)
 soup = BeautifulSoup(page.content, "html.parser")
+denver = open("Denver.txt", 'a')
 
 while True:
+    #Open file
+    with open('Denver.txt') as file:
+        denver = open("Denver.txt", 'a')
     try:
-        high = 0
-        low = 0
+        #Find Daily High and Lows
         info = soup.find('body')
         info = info.find('div', id='seven-day-forecast')
         info = info.find('div', id='seven-day-forecast-body')
         info = info.find('div', id='seven-day-forecast-container')
         info = info.find('ul', id='seven-day-forecast-list')
-        info = info.find('li')
-        list = info.find_all('p')
-        for l in list:
-            print(l)
-            if "High" in l:
-                high = l
-            elif "Low" in l:
-                low = l
-        print(high)
-        print(low)
-    except RuntimeError:
+        info = info.find_all('li')
+        for i in info:
+            list = i.find_all('p')
+            for l in list:
+                if "temp temp-high" in str(l):
+                    high = str(l)
+                    high = high[33:37]
+                elif "temp temp-low" in str(l):
+                    low = str(l)
+                    low = low[30:35]
+
+        #Find current temp
+        info = soup.find('body')
+        info = info.find('div', id='current-conditions')
+        info = info.find('div', id='current-conditions-body')
+        info = info.find('div', id='current_conditions-summary')
+        info = info.find_all('p')
+        for i in info:
+            if "myforecast-current-lrg" in str(i):
+                current = str(i)
+                current = current[34:38]
+
+        #Find date
+        dateToday = date.today()
+
+        #Find time
+        clockNow = datetime.now()
+
+        #Add information to file
+        print(f"{clockNow}, {dateToday}, {current}, {high}, {low}")
+        denver.write(f"{clockNow}, {dateToday}, {current}, {high}, {low}")
+    except:
         pass
-    time.sleep(3)
+    
+    time.sleep(1440)
